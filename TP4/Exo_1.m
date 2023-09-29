913e96e49c44130418eff739753d85b27dc11f09
eyal.grs <eyalgrosdaille@gmail.com>
2023-09-29 14:45:49 +0200

    Fixed TP names + Added 4th one

---
+++ TP4/Exo_1.m
@@ -0,0 +1,66 @@
+﻿clear all;
+close all;
+
+img = imread('exo1.jpeg');
+
+if size(img, 3) == 3
+    img = rgb2gray(img);
+end
+
+image_stretched = imadjust(img, stretchlim(img), []);
+
+image_equalized = histeq(image_stretched);
+
+[NL, NC] = size(image_equalized);
+image_denoized = zeros(NL, NC);
+
+filter_size = 3;
+
+padding = floor(filter_size / 2);
+
+for y = 2:NL-1  % Avoid processing the image boundaries
+    for x = 2:NC-1
+        A = image_equalized(y-1:y+1, x-1:x+1);
+        mid_val = median(A(:));
+        image_denoized(y, x) = mid_val;
+    end
+end
+
+% Create a figure
+figure;
+
+% Display the Original Image and its histogram
+subplot(2, 4, 1);
+imshow(img);
+title('Original Image');
+
+subplot(2, 4, 5);
+imhist(img);
+title('Histogram');
+
+% Display the Stretched Image and its histogram
+subplot(2, 4, 2);
+imshow(image_stretched);
+title('Stretched Image');
+
+subplot(2, 4, 6);
+imhist(image_stretched);
+title('Histogram');
+
+% Display the Equalized Image and its histogram
+subplot(2, 4, 3);
+imshow(image_equalized);
+title('Equalized Image');
+
+subplot(2, 4, 7);
+imhist(image_equalized);
+title('Histogram');
+
+% Display the Equalized Image and its histogram
+subplot(2, 4, 4);
+imshow(image_denoized);
+title('Denoised Image');
+
+subplot(2, 4, 8);
+imhist(image_denoized);
+title('Histogram');
---
+++ TP4/Exo_2.m
@@ -0,0 +1,71 @@
+﻿clear all;
+close all;
+
+% Charger l'image originale
+I = imread('lena.jpg');
+
+% Ajouter du bruit gaussien et du bruit salt & pepper
+I_gaussian = imnoise(I, 'gaussian', 0, 0.04);
+I_sp = imnoise(I, 'salt & pepper', 0.04);
+
+
+%Below is the algorithm for an average filter
+
+%[NL, NC] = size(I_sp);
+%image_denoized2 = zeros(NL, NC);
+
+%for y = 2:NL-1  % Avoid processing the image boundaries
+%    for x = 2:NC-1
+%        A = I_sp(y-1:y+1, x-1:x+1);
+%        avg_val = sum(sum(A))/9;
+%        image_denoized2(y, x) = avg_val;
+%    end
+%end
+
+filter_size = 3;
+
+% Appliquer un filtre moyenneur rectangulaire 3x3 sur les images avec bruit
+filter = fspecial('average', [filter_size, filter_size]);
+I_gaussian_filtered = imfilter(I_gaussian, filter);
+I_sp_filtered = imfilter(I_sp, filter);
+
+
+% Appliquer un filtre médian sur l'image avec bruit salt & pepper
+I_gaussian_median = medfilt2(I_gaussian, [filter_size, filter_size]);
+% Appliquer un filtre médian sur l'image avec bruit salt & pepper
+I_sp_median = medfilt2(I_sp, [filter_size, filter_size]);
+
+% Afficher l'image originale
+figure;
+imshow(I);
+title('Image originale');
+
+% Afficher l'image avec bruit gaussien
+figure;
+imshow(I_gaussian);
+title('Image Bruit Gaussien');
+
+% Afficher l'image avec bruit salt & pepper
+figure;
+imshow(I_sp);
+title('Bruit Salt & Pepper');
+
+% Afficher l'image avec bruit gaussien filtrée par moyenneur
+figure;
+imshow(I_gaussian_filtered);
+title('Bruit Gaussien (Filtre Moyenneur)');
+
+% Afficher l'image avec bruit salt & pepper filtrée par moyenneur
+figure;
+imshow(I_sp_filtered);
+title('Salt & Pepper (Filtre Moyenneur)');
+
+% Afficher l'image avec bruit gaussien filtrée par médian
+figure;
+imshow(I_gaussian_median);
+title('Gaussien (Filtre Médian)');
+
+% Afficher l'image avec bruit salt & pepper filtrée par médian
+figure;
+imshow(I_sp_median);
+title('Salt & Pepper (Filtre Médian)');
